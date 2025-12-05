797738b924f9c0d585d31afc0e54f8df8ae023df
arppuri <anu.nissinen@helsinki.fi>
2025-12-05 12:28:55 +0200

    vko 6 nhl

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,21 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
+from querybuilder import QueryBuilder
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+
+    query = QueryBuilder()
+
+    matcher = query.plays_in("NYR").has_at_least(10, "goals").build()
+
+    for player in stats.matches(matcher):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
